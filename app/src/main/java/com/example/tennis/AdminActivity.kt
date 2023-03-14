80df6a92a331e63e4234267be90081a8413f6a9d
fanny2010 <67238222+fanny2010@users.noreply.github.com>
2023-03-14 18:53:49 +0100

    V3

--- app/src/main/java/com/example/tennis/AdminActivity.kt
+++ app/src/main/java/com/example/tennis/AdminActivity.kt
@@ -1,22 +1,5 @@
 package com.example.tennis
 
-<<<<<<< Updated upstream
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.tennis.model.User
-import com.example.tennis.model.UserRole
-import com.google.firebase.auth.FirebaseUser
-=======
-import com.example.tennis.UserRole
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -29,9 +12,9 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tennis.model.User
 import com.example.tennis.model.UserRole
 import com.google.firebase.auth.FirebaseAuth
->>>>>>> Stashed changes
 import com.google.firebase.database.*
 
 
@@ -39,15 +22,9 @@ import com.google.firebase.database.*
 class AdminActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-<<<<<<< Updated upstream
-    private lateinit var users: MutableList<User>
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var userAdapter: UserAdapter
-=======
     private lateinit var adapter: UsersListAdapter
     private lateinit var usersList: RecyclerView
     private lateinit var usersRef: DatabaseReference
->>>>>>> Stashed changes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,73 +45,6 @@ class AdminActivity : AppCompatActivity() {
             finish()
         }
 
-<<<<<<< Updated upstream
-        // Charger les utilisateurs de la base de données ou de toute autre source
-                users = mutableListOf(
-                    User(1, "Alice", UserRole.MEMBER),
-                    User(2, "Bob", UserRole.ADHERENT),
-                    User(3, "Charlie", UserRole.MEMBER),
-                    User(4, "Dave", UserRole.MEMBER),
-                    User(5, "Eve", UserRole.ADHERENT)
-                )
-
-                // Configurer le RecyclerView pour afficher la liste des utilisateurs
-                recyclerView = findViewById(R.id.recyclerView)
-                userAdapter = UserAdapter(users)
-                recyclerView.adapter = userAdapter
-                recyclerView.layoutManager = LinearLayoutManager(this)
-            }
-
-            inner class UserAdapter(private val userList: List<User>) :
-                RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
-
-                inner class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-                    val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
-                    val roleTextView: TextView = itemView.findViewById(R.id.roleTextView)
-                    val memberButton: Button = itemView.findViewById(R.id.button_membre)
-                    val adherentButton: Button = itemView.findViewById(R.id.button_adherent)
-                }
-
-                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-                    val itemView = LayoutInflater.from(parent.context)
-                        .inflate(R.layout.item_user, parent, false)
-                    return UserViewHolder(itemView)
-                }
-
-                override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-                    val currentUser = userList[position]
-                    holder.nameTextView.text = currentUser.name
-                    holder.roleTextView.text = currentUser.role.toString()
-
-                    if (isAdmin()) {
-                        // Permettre à l'administrateur de modifier le rôle de l'utilisateur en appuyant sur les boutons "Membre" ou "Adhérent"
-                        holder.memberButton.setOnClickListener {
-                            currentUser.role = UserRole.MEMBER
-                            holder.roleTextView.text = currentUser.role.toString()
-                        }
-                        holder.adherentButton.setOnClickListener {
-                            currentUser.role = UserRole.ADHERENT
-                            holder.roleTextView.text = currentUser.role.toString()
-                        }
-                    } else {
-                        // Désactiver les boutons "Membre" et "Adhérent" pour les utilisateurs non-administrateurs
-                        holder.memberButton.isEnabled = false
-                        holder.adherentButton.isEnabled = false
-                    }
-                }
-
-                override fun getItemCount(): Int {
-                    return userList.size
-                }
-            }
-
-            private fun isAdmin(): Boolean {
-                // Remplacer ceci par votre vérification de rôle d'administrateur
-                return true
-            }
-}
-
-=======
         usersList = findViewById(R.id.users_list)
         usersList.layoutManager = LinearLayoutManager(this)
 
@@ -216,5 +126,4 @@ class UsersListAdapter(private val users: List<User>) :
     }
 
 }
->>>>>>> Stashed changes
 
