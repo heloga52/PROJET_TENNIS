fc5d9e20fc3601ca8e2dff077eda138af48f8a23
heloga52 <67238226+heloga52@users.noreply.github.com>
2023-03-14 23:25:46 +0100

    v4

--- app/src/main/java/com/example/tennis/AdherentActivity.kt
+++ app/src/main/java/com/example/tennis/AdherentActivity.kt
@@ -1,4 +1,31 @@
 package com.example.tennis
 
-class AdherentActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+
+class AdherentActivity : AppCompatActivity() {
+
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adherent)
+        mAuth = FirebaseAuth.getInstance()
+
+        // Récupération du bouton de déconnexion
+        val signOutButton: Button = findViewById(R.id.logout_button)
+        signOutButton.setOnClickListener {
+            // Déconnexion de l'utilisateur
+            mAuth.signOut()
+            // Redirection vers la page de login
+            val intent = Intent(this@AdherentActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
 }
