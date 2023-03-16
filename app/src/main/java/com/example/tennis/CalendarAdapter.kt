08400e167556e050e9235451c032766f8a4c81f4
heloga52 <67238226+heloga52@users.noreply.github.com>
2023-03-16 22:08:32 +0100

    V6 comme les mustang lol

--- app/src/main/java/com/example/tennis/CalendarAdapter.kt
+++ app/src/main/java/com/example/tennis/CalendarAdapter.kt
@@ -3,6 +3,7 @@ package com.example.tennis
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tennis.databinding.CellDayBinding
@@ -14,7 +15,8 @@ class CalendarAdapter(val context: Context, val clickListener: (Reservation) -> 
     val firstDayIndex: Int
 
     init {
-        val calendar: Calendar = Calendar.getInstance()
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, 7)
         val day: Int = calendar.get(Calendar.DAY_OF_WEEK)
         firstDayIndex = day -1
     }
@@ -37,30 +39,51 @@ class CalendarAdapter(val context: Context, val clickListener: (Reservation) -> 
 
     override fun onBindViewHolder(holder: CellDayViewHolder, position: Int) {
         val df = SimpleDateFormat("yyyy-MM-dd", Locale.FRANCE)
-        val date = Date(Date().getTime() + (position * 1000 * 60 * 60 * 24))
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.add(Calendar.DAY_OF_MONTH, position - firstDayIndex)
+        val datePlusSixHours = calendar.time
+        val date = Date(datePlusSixHours.time)
+        date.time = date.time + (position - firstDayIndex) * 24 * 60 * 60 * 1000
 
-        val dfHour = SimpleDateFormat("HH", Locale.FRANCE)
-        val dfMonth = SimpleDateFormat("MM", Locale.FRANCE)
-        val dfYear = SimpleDateFormat("YYYY", Locale.FRANCE)
-        val dfDay = SimpleDateFormat("dd", Locale.FRANCE)
-        val year = dfYear.format(date)
-        val month = dfMonth.format(date)
-        val hour = dfHour.format(date)
-        val id = UUID.randomUUID().toString()
-        val day = dfDay.format(date)
 
         holder.dayTitle.text = days[(position + firstDayIndex) % days.count()]
         holder.terrain1.layoutManager = GridLayoutManager(context, 5)
         holder.terrain1.adapter = TerrainAdapter() {
+            val dfHour = SimpleDateFormat("HH", Locale.FRANCE)
+            val dfMonth = SimpleDateFormat("MM", Locale.FRANCE)
+            val dfYear = SimpleDateFormat("YYYY", Locale.FRANCE)
+            val dfDay = SimpleDateFormat("dd", Locale.FRANCE)
+            val year = dfYear.format(date)
+            val month = dfMonth.format(date)
+            val hour = (it + 7) % 24
+            val id = UUID.randomUUID().toString()
+            val day = dfDay.format(date)
 
-            val reservation = Reservation(id, day,hour.toInt() ,month ,year , "1", "")
-            clickListener(reservation)
+            if (hour in 10..17) {
+                Toast.makeText(context, "Les réservations sont impossibles de 10h à 18h.", Toast.LENGTH_LONG).show()
+            } else {
+                val reservation = Reservation(id, day, hour.toInt(), month, year, "1", "")
+                clickListener(reservation)
+            }
         }
 
         holder.terrain2.layoutManager = GridLayoutManager(context, 5)
         holder.terrain2.adapter = TerrainAdapter() {
-            val reservation = Reservation(id, day,hour.toInt() ,month ,year , "2", "")
-            clickListener(reservation)
-        }
+            val dfHour = SimpleDateFormat("HH", Locale.FRANCE)
+            val dfMonth = SimpleDateFormat("MM", Locale.FRANCE)
+            val dfYear = SimpleDateFormat("YYYY", Locale.FRANCE)
+            val dfDay = SimpleDateFormat("dd", Locale.FRANCE)
+            val year = dfYear.format(date)
+            val month = dfMonth.format(date)
+            val hour = (it + 7) % 24
+            val id = UUID.randomUUID().toString()
+            val day = dfDay.format(date)
+            if (hour in 10..17) {
+                Toast.makeText(context, "Les réservations sont impossibles de 10h à 18h.", Toast.LENGTH_LONG).show()
+            } else {
+                val reservation = Reservation(id, day, hour.toInt(), month, year, "2", "")
+                clickListener(reservation)
+            }}
     }
+
 }
